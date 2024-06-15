8fbb9c224df6ae952d447ff67e9766d83100efd4
icefarelli <ivan.cefarelli@docentes.mdp.utn.edu.ar>
2024-06-15 10:01:29 -0300

    Contructor, get y set Plato

--- src/main/java/Plato/Plato.java
+++ src/main/java/Plato/Plato.java
@@ -5,6 +5,24 @@ public class Plato {
     private double precio;
 
 
+    public Plato(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
